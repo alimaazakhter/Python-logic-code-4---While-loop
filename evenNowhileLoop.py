6e35b7a03a5bc584b798756ea44a101e45b60288
Alimaaz Akhter <150456791+alimaazakhter@users.noreply.github.com>
2025-09-17 12:03:39 +0530

    Update evenNowhileLoop.py

--- evenNowhileLoop.py
+++ evenNowhileLoop.py
@@ -8,3 +8,13 @@ while(n1<=end):
     if(n1%2==0):
         print(f"Even number: {n1}")
     n1+=1    
+
+#even=int(input("Enter number : "))
+#print(f"Even number till {even} : ")
+#no=1
+#while(no<=even):
+#    if(no%2==0):
+#        print("Even: ", no)
+#    else:
+#       print("odd: ", no)
+#    no+=1 #increment 
