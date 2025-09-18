72399786a129eadc40148790bc394a58c2b4c32f
Alimaaz Akhter <150456791+alimaazakhter@users.noreply.github.com>
2025-09-18 09:43:29 +0530

    Add files via upload

---
+++ WhileTrue-addTwoNos.py
@@ -0,0 +1,9 @@
+while True:
+    num1 = int(input("Enter any number: "))
+    num2 = int(input("Enter any number: "))
+
+    print(num1+num2)
+
+    repeat = input("You want to repeat the program (yes/no) : ")
+    if repeat=="yes":
+        break
---
+++ whiletru.py
@@ -0,0 +1,19 @@
+#WHILE TRUE LOOP IMPLEMENTION
+while True:
+    num1 = int(input("Enter any number :"))
+    num2 = int(input("Enter any number :"))
+
+    print(num1+num2)
+    repeat = input('do you want to stop2 the program: ')
+    if repeat == "yes":
+        break
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#while True:
+#    print("Hello world")
+------------------------------------------------------------------------------------------------------------------------------------------
+
+#n=1
+#while True:
+    # print(n)
+    # n +=1
